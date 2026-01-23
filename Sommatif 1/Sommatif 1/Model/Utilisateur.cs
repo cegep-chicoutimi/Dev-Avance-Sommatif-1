847b2063f0751ed3fc47f3cf87ddaf1f68404d97
Aguibou Balde <2311029@etu.cchic.ca>
2026-01-23 10:51:51 -0500

    Premier commit

---
+++ Sommatif 1/Sommatif 1/Model/Utilisateur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sommatif_1.Model
+{
+    public class Utilisateur
+    {
+        public string User {  get; set; }
+        public string Password { get; set; }
+
+        public Utilisateur(string user, string password)
+        {
+            User = user;
+            Password = password;
+        }
+    }
+}
---
+++ Sommatif 1/Sommatif 1/View-Model/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sommatif_1.View_Model
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Se produit lorsque la valeur d'une propriété change.
+        /// </summary>
+        /// <remarks>
+        /// Cet événement est généralement déclenché par l'implémentation de l'interface 
+        /// <see cref="INotifyPropertyChanged"/> qui notifie les abonnés qu'une valeur de 
+        /// propriété a été mise à jour. Utilisez cet événement pour surveiller les 
+        /// changements de propriétés dans la classe implémentante.
+        /// </remarks>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Lève l'événement <see cref="PropertyChanged"/> pour notifier les abonnés 
+        /// qu'une valeur de propriété a changé.
+        /// </summary>
+        /// <remarks>
+        /// Cette méthode doit être appelée chaque fois qu'une valeur de propriété est 
+        /// mise à jour afin de s'assurer que les liaisons de données et autres écouteurs 
+        /// sont informés du changement.
+        /// <param name="name">Nom de la propriété qui a changé.</param>
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    }
+}
